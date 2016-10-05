4bb87441c704fbfc12068fb66bcc6c91f51ab0a2
Amal Gamage <amal@entrusttitle.com>
2016-10-05 17:13:44 +0530

    Initialize RunMeteor Basic Skeleteon

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Mocha
+        mocha: {
+            all: {
+                src: ['tests/testconsole.html'],
+            },
+            options: {
+                run: true
+            }
+        }
+    });
+
+    // Load grunt mocha task
+    grunt.loadNpmTasks('grunt-mocha');
+
+    grunt.registerTask('default', ['mocha']);
+};
---
+++ src/index.js
@@ -0,0 +1,21 @@
+console.log("Executing RunMeteor...");
+
+module.exports = {
+    escape: function(html) {
+        return String(html)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    },
+
+    unescape: function(html) {
+        return String(html)
+            .replace(/&amp;/g, '&')
+            .replace(/&quot;/g, '"')
+            .replace(/&#39;/g, ''')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>');
+    }
+};
