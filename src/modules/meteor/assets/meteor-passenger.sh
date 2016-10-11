64e8654f332b5ee9890c1fbba63f33723b13df84
Amal Gamage <amal@entrusttitle.com>
2016-10-11 15:44:09 +0530

    Build location corrected

--- src/modules/meteor/assets/meteor-passenger.sh
+++ src/modules/meteor/assets/meteor-passenger.sh
@@ -7,6 +7,20 @@ ENV_FILE=$APP_PATH/config/env.list
 PORT=<%= port %>
 PASSENGER_JSON_PATH=$APP_PATH/config/Passengerfile.json
 
+set -e
+
+# save the last known version
+cd $APP_PATH
+if [[ -d current ]]; then
+  sudo rm -rf last
+  sudo mv current last
+fi
+
+# setup the new version
+sudo mkdir current
+sudo cp tmp/bundle.tar.gz current/
+
+# start building meteor bundle
 cd $BUNDLE_PATH
 sudo chmod 777 bundle.tar.gz
 sudo tar xzf bundle.tar.gz
